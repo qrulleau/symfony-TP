4819c81c3ee78a2eeabae2265d440eea0ec83c2a
qrulleau <quentin.rulleau@yahoo.com>
2022-11-21 15:20:25 +0100

    ADD: js/css file and start transfering php project

---
+++ templates/includes/nav.php
@@ -0,0 +1,20 @@
+<div class="background">
+    <div class="d-flex direction-column align-start nav-bar">
+        <a href="http://localhost/project-management/index.php" class="d-flex new-width justify-start icon">
+            <i data-feather="home"></i>
+            <p class="nav-text d-none">Tableau de bord</p>
+        </a>
+        <a href="http://localhost/project-management/view/project.php" class="d-flex new-width justify-start icon">
+            <i data-feather="folder"></i>
+            <p class="nav-text d-none">Projets</p>
+        </a>
+        <a href="http://localhost/project-management/view/client.php" class="d-flex new-width justify-start icon ">
+            <i data-feather="user"></i>
+            <p class="nav-text d-none">Client</p>
+        </a>
+        <a href="http://localhost/project-management/view/host.php" class="d-flex new-width justify-start icon">
+            <i data-feather="cloud"></i>
+            <p class="nav-text d-none">Hebergeur</p>
+        </a>
+    </div>
+</div>
