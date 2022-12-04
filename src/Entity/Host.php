c6736c5a06933f3a1e6e59c3307a8d8fb9d0a1ec
qrulleau <quentin.rulleau@yahoo.com>
2022-12-04 14:44:31 +0100

    ADD: orm entity on each entity and foreign key

--- src/Entity/Host.php
+++ src/Entity/Host.php
@@ -2,10 +2,27 @@
 
 namespace App\Entity;
 
-use Exception;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\HostRepository;
 
+#[ORM\Entity(repositoryClass: HostRepository::class)]
 class Host
 {
+
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: 'integer')]
+  private int $id;
+
+  #[ORM\Column(type: 'text' ,length: 255)]
+  private string $code;
+
+  #[ORM\Column(type: 'text' ,length: 255)]
+  private string $name;
+
+  #[ORM\Column(type: 'text')]
+  private string $notes;
+
   public static function getAllHosts()
   {
 
--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -2,10 +2,53 @@
 
 namespace App\Entity;
 
-use Exception;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ProjectRepository;
+use App\Entity\Customer;
+use App\Entity\Host;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
 
 class Project
 {
+
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: 'integer')]
+  private int $id;
+
+
+  #[ORM\Column(type: 'string', length: 255)]
+  private string $name;
+
+  #[ORM\Column(type: 'string', length: 255)]
+  private string $code;
+
+  #[ORM\Column(type: 'string', length: 255)]
+  private string $lasspass_folder;
+
+  #[ORM\Column(type: 'string', length: 255)]
+  private string $link_mock_ups;
+
+  #[ORM\Column(type: 'integer')]
+  private int $managed_server;
+
+  #[ORM\Column(type: 'text')]
+  private string $notes;
+
+  #[ORM\ManyToOne(targetEntity: Customer::class)]
+  #[ORM\JoinColumn(name:"customer", referencedColumnName:"id")]
+  private Customer $customer;
+
+  #[ORM\ManyToOne(targetEntity: Host::class)]
+  #[ORM\JoinColumn(name:"host", referencedColumnName:"id")]
+  private Customer $host;
+
+  // #[ManyToOne(targetEntity: Address::class)]
+  // #[JoinColumn(name: 'address_id', referencedColumnName: 'id')]
+  // private Customer  $customer ;
+
+
   public static function getAllProjects()
   {
 
